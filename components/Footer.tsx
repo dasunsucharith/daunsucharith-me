492b461bcf442c145315dd47ac923f3012e4f8fc
Dasun Sucharith <sucharith.dasun@gmail.com>
2025-09-01 12:55:51 +0530

    style(footer): match width to header container

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -78,7 +78,7 @@ const Footer = () => {
 
       <div className="relative z-10">
         {/* Main footer content */}
-        <div className="max-w-6xl mx-auto px-8 py-16">
+        <div className="container mx-auto px-4 py-16">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
             
             {/* Brand section */}
